661fe13a2588ffcbd0532592ebd6c6781431d039
lorenacorrea2022@hotmail.com <lorenacorrea2022@hotmail.com>
2026-04-17 19:11:31 -0500

    Corrijo challenges y validaciones

--- final/Challenges/Challenge7.cs
+++ final/Challenges/Challenge7.cs
@@ -1,25 +1,21 @@
-using System;
+Console.Write("Ingrese 3 números:\n");
 
-namespace final
+int a, b, c;
+
+Console.Write("Número 1: ");
+while (!int.TryParse(Console.ReadLine(), out a))
 {
-    public class Challenge7
-    {
-        public void Run()
-        {
-            Console.Write("Ingrese 3 números:\n");
+    Console.Write("Ingrese un número válido: ");
+}
 
-            Console.Write("Número 1: ");
-            int a = int.Parse(Console.ReadLine());
+Console.Write("Número 2: ");
+while (!int.TryParse(Console.ReadLine(), out b))
+{
+    Console.Write("Ingrese un número válido: ");
+}
 
-            Console.Write("Número 2: ");
-            int b = int.Parse(Console.ReadLine());
-
-            Console.Write("Número 3: ");
-            int c = int.Parse(Console.ReadLine());
-
-            double promedio = (a + b + c) / 3.0;
-
-            Console.WriteLine($"El promedio es: {promedio}");
-        }
-    }
+Console.Write("Número 3: ");
+while (!int.TryParse(Console.ReadLine(), out c))
+{
+    Console.Write("Ingrese un número válido: ");
 }
--- final/Program.cs
+++ final/Program.cs
@@ -1,1 +1,27 @@
-﻿Console.WriteLine("Hello, World!");
+﻿
+using System;
+
+namespace final
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Selecciona un challenge (6-13): ");
+            int option = int.Parse(Console.ReadLine());
+
+            switch(option)
+            {
+                case 6: new Challenge6().Run(); break;
+                case 7: new Challenge7().Run(); break;
+                case 8: new Challenge8().Run(); break;
+                case 9: new Challenge9().Run(); break;
+                case 10: new Challenge10().Run(); break;
+                case 11: new Challenge11().Run(); break;
+                case 12: new Challenge12().Run(); break;
+                case 13: new Challenge13().Run(); break;
+                default: Console.WriteLine("Opción no válida"); break;
+            }
+        }
+    }
+}
