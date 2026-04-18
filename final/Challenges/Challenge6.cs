093d34fac4dfc623ba45f10ff8bf9c5419c5370c
lorenacorrea2022@hotmail.com <lorenacorrea2022@hotmail.com>
2026-04-17 19:02:46 -0500

    Actualizo challenges y arreglo proyecto

--- final/Challenges/Challenge6.cs
+++ final/Challenges/Challenge6.cs
@@ -1,9 +1,20 @@
-
 using System;
-namespace final {
-    public class Challenge6 {
-        public void Run() {
-            Console.WriteLine("Ejecutando Challenge 6");
+
+namespace FinalProject.Challenges
+{
+    public class Challenge6
+    {
+        public void Run()
+        {
+            Console.Write("Ingrese un número: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Tabla del {numero}:");
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"{numero} x {i} = {numero * i}");
+            }
         }
     }
 }
--- final/Challenges/Challenge7.cs
+++ final/Challenges/Challenge7.cs
@@ -1,9 +1,25 @@
-
 using System;
-namespace final {
-    public class Challenge7 {
-        public void Run() {
-            Console.WriteLine("Ejecutando Challenge 7");
+
+namespace final
+{
+    public class Challenge7
+    {
+        public void Run()
+        {
+            Console.Write("Ingrese 3 números:\n");
+
+            Console.Write("Número 1: ");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.Write("Número 2: ");
+            int b = int.Parse(Console.ReadLine());
+
+            Console.Write("Número 3: ");
+            int c = int.Parse(Console.ReadLine());
+
+            double promedio = (a + b + c) / 3.0;
+
+            Console.WriteLine($"El promedio es: {promedio}");
         }
     }
 }
